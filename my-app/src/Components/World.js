bce1953835d67929d0a7ad607ef920707838fb48
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2019-08-27 17:44:58 -0700

    update

--- my-app/src/Components/World.js
+++ my-app/src/Components/World.js
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
-export default class World extends Component {
+//
+// axios call to /adv/init to initialize world
+// display response info
+
+// Create an interface that displays the current room name, its description and the other players in the room
+
+
+export default class World extends React.Component {
+    state = {
+        players: ["Outside Cave Entrance", "Rosa's Room"]
+    }
+
+    componentDidMount() {
+        this.setState({ loading: true });
+        axios.get(`https://lambda-mud-test.herokuapp.com/api/adv/init/`)
+            .then(res => {
+                console.log('res', res);
+                this.setState({ players: res.data });
+                console.log('players', res.data)
+            }).catch(err => err.res)
+    }
     render(){
         return(
-            <div>Hello</div>
+            <div>
+                <h1>In the world</h1>
+                <ul>
+                    {this.state.players.map(room => <li>{room.players}</li>)}
+                </ul>
+            </div>
         )
     }
 };
+
