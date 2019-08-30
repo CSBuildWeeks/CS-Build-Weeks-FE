6429db90b5aea1f8d0358bdc5f5f2cd7c5e7b5ff
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2019-08-29 19:01:23 -0700

    linked to heruko deployed API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,9 +8,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      {/* <Legend>
-          Lambda Multi-User Dungeon (MUD)
-        </Legend> */}
       <AppRouter />
       
         <a
@@ -19,10 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {/* Login  */}
         </a>
         <div className='register'>
-          {/* <Register /> */}
         </div>
       </header>
     </div>
@@ -30,17 +25,3 @@ function App() {
 }
 
 export default App;
-
-// // Styled Components
-// const Legend = styled.legend`
-// display: contents;
-// width: 40rem;
-// margin-top: 1rem;
-// height: 5rem;
-// font-size: 39px;
-// margin: 26px;
-// color: white;
-// border-radius: 8px;
-// border: 1px solid white;
-// padding-top: 2rem;
-// `
